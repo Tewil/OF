829856c7f726e067c1ec10fe1c0b42b59923f0d6
Tewil <68031256+Tewil@users.noreply.github.com>
2021-07-07 15:31:08 +0200

    checkout 7.7. noon
    Added List for pngs plus cript to create
    added script for printing png to craft panel // not finished

---
+++ Assets/Scripts/PNGGenerate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PNGGenerate : MonoBehaviour
+{
+	public Texture png;
+	
+	public Image myImageComponent;
+	public Image x;
+	
+	public Sprite mySprite;
+	
+	void Start()
+    {
+        GameObject thePlayer = GameObject.Find("ThePlayer");
+        PNGListCreator PNGScript = thePlayer.GetComponent<PNGListCreator>();
+		png = PNGScript.pngs[5];
+        
+    }
+	
+    public void generatePNG() 
+	{
+		
+		myImageComponent = this.GetComponent<Image>();
+		//TODO:
+		//Print png to myImageComponent = Source Image of Image in panel
+		//myImageComponent.sprite = png;
+		
+		
+	}
+}
---
+++ Assets/Scripts/PNGListCreator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNGListCreator : MonoBehaviour
+{
+	
+	public Texture[] pngs;
+    // Start is called before the first frame update
+    
+}
